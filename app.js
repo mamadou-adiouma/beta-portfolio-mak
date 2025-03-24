406b9e1d572daa57ee1e53585e0b0146f4cae045
mamadou adiouma <adiou.xyz@gmail.com>
2025-03-24 02:08:46 +0000

    Beta V1 projet portfolio

---
+++ app.js
@@ -0,0 +1,24 @@
+const parentHTML = document.documentElement,
+     themeBtn = document.getElementById('themeBtn'),
+     checkTheme = document.getElementById('checkTheme');
+
+let light = window.matchMedia('(prefers-color-scheme: light)').matches;
+document.addEventListener('DOMContentLoaded', () => {
+     if (!light) {
+          parentHTML.dataset.theme = 'dark';
+     } else {
+          parentHTML.dataset.theme = 'light'
+     }
+
+     const popUp = () => {
+          setTimeout(() => {
+               document.querySelector('.mssg').style.display = 'grid';
+          }, 2000);
+     }
+
+     setTimeout(() => {
+          document.querySelector('.mssg').style.display = 'none';
+     }, 10000);
+
+     window.addEventListener('DOMContentLoaded', popUp)
+})
